d0eb278960070c9dc0089737603c6f6977a90c08
Phan Thanh Duong <0306221410@caothang.edu.vn>
2025-01-12 23:08:31 +0700

    Update all file UI KingOfShoes

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-//import 'package:kingofshoes/views/Login_Screen.dart';
-//import 'package:kingofshoes/views/ProfileScreen.dart';
-import 'package:kingofshoes/views/QuanLyDonHang.dart';
-import 'package:kingofshoes/viewmodels/QuanLyHoaDon.dart';
-import 'package:provider/provider.dart';
+// import 'package:flutter/material.dart';
+// //import 'package:kingofshoes/views/Login_Screen.dart';
+// //import 'package:kingofshoes/views/ProfileScreen.dart';
+// import 'package:kingofshoes/views/QuanLyDonHang.dart';
+// import 'package:kingofshoes/viewmodels/QuanLyHoaDon.dart';
+// import 'package:provider/provider.dart';
 
-void main() {
-  Provider.debugCheckInvalidValueType = null;
-  runApp(MainApp());
-}
+// void main() {
+//   Provider.debugCheckInvalidValueType = null;
+//   runApp(MainApp());
+// }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Provider<OrderViewModel>(
-      create: (_) => OrderViewModel(),
-      builder: (context, child) {
-        return QuanLyDonHangScreen();
-      },
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Provider<OrderViewModel>(
+//       create: (_) => OrderViewModel(),
+//       builder: (context, child) {
+//         return QuanLyDonHangScreen();
+//       },
+//     );
+//   }
+// }
